2a66a982144db1b67f683c59c6c70fa9f512883e
Greeper-T <hyrtwisback@gmail.com>
2024-10-16 19:21:14 -0500

    changed image for banner

--- personal-portfollio/src/components/Banner.js
+++ personal-portfollio/src/components/Banner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from '../assets/img/header-img.svg';
+import headerImg from '../assets/img/video-game.png';
 
 
 function Banner (){
